cfa743d7428ed36003422607ff10cf488d196e81
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-11 10:06:01 -0400

    Implement Select

--- S24W9ConnectedModel/MainWindow.xaml.cs
+++ S24W9ConnectedModel/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace S24W9ConnectedModel
 {
@@ -19,6 +21,66 @@ namespace S24W9ConnectedModel
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(Data.ConnectionStr))
+            {
+                string query = "select EmployeeID, FirstName, LastName, City from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+            //conn.Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadGrid();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Data.ConnectionStr))
+            {
+                // string concatenation - DO Not Use
+                //string query = "select EmployeeID, FirstName, LastName, City from Employees where FirstName='" + txtFirstname.Text + "'";
+
+                // parameterized query - Use This
+                string query = "select EmployeeID, FirstName, LastName, City from Employees where FirstName=@fn";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@fn", txtFirstname.Text);
+
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable tbl = new DataTable();
+                tbl.Load(reader);
+
+                grdEmployees.ItemsSource = tbl.DefaultView;
+            }
+        }
+
+        private void btnCount_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Data.ConnectionStr))
+            {
+                string query = "select Count(*) from Employees";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                conn.Open();
+
+                int numRows = (int)cmd.ExecuteScalar();
+
+                MessageBox.Show("Number of rows = " + numRows);
+            }
         }
     }
 }

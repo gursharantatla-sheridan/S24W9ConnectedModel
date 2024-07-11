f30030dbca8690bd1047440a5f6c98f4c97c2ca1
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-11 09:05:54 -0400

    Design UI

---
+++ S24W9ConnectedModel/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24W9ConnectedModel
+{
+    public class Data
+    {
+        private static string connectionStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+
+        public static string ConnectionStr { get { return connectionStr; } }
+    }
+}
